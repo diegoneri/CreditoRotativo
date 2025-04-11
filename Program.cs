36440bf14687924762177a0ce981420f85f7a599
Diego Neri <diego.neri@etec.sp.gov.br>
2025-04-11 20:47:22 -0300

    Finaliza programa com multa por atraso

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿const double taxaIOFAoMes = 0.0038;
+const double taxaIOFAoDia = 0.000082;
+
+decimal valorTotalFatura, 
+        percentualPagamentoMinimo,
+        percentualMultaAtraso, 
+        taxaJurosMensais,
+        valorPagamentoMinimo,
+        valorNaoPago,
+        jurosSobreRotativo,
+        valorIOFMensal,
+        valorIOFDiario,
+        valorMultaAtraso,
+        valorProximaFatura,
+        custoCreditoRotativo;
+
+Console.Clear();
+Console.WriteLine("--- Crédito Rotativo de Cartão de Crédito ---");
+
+Console.Write($"Valor total da fatura (R$)...: ");
+valorTotalFatura = Convert.ToDecimal(Console.ReadLine());
+Console.Write($"Pagamento mínimo (%).........: ");
+percentualPagamentoMinimo = Convert.ToDecimal(Console.ReadLine()) / 100;
+Console.Write($"Taxa de juros mensais (%)....: ");
+taxaJurosMensais = Convert.ToDecimal(Console.ReadLine()) / 100;
+
+Console.Write($"Multa de atraso (%)....: ");
+percentualMultaAtraso = Convert.ToDecimal(Console.ReadLine()) / 100;
+
+Console.WriteLine();
+valorPagamentoMinimo = valorTotalFatura * percentualPagamentoMinimo; 
+Console.WriteLine($"Pagamento mínimo..................: {valorPagamentoMinimo:C2}");
+
+Console.WriteLine("\nCaso seja pago o valor mínimo:\n");
+
+valorNaoPago = valorTotalFatura - valorPagamentoMinimo;
+Console.WriteLine($"Valor não pago....................: {valorNaoPago:C2}");
+
+jurosSobreRotativo = valorNaoPago * taxaJurosMensais;
+Console.WriteLine($"Juros.............................: {jurosSobreRotativo:C2}");
+
+valorIOFMensal = valorNaoPago * Convert.ToDecimal(taxaIOFAoMes);
+Console.WriteLine($"IOF mensal........................: {valorIOFMensal:C2}");
+
+valorIOFDiario = valorNaoPago * Convert.ToDecimal(taxaIOFAoDia * 30);
+Console.WriteLine($"IOF diário........................: {valorIOFDiario:C2}");
+
+valorMultaAtraso = valorNaoPago * percentualMultaAtraso;
+Console.WriteLine($"Multa por atraso..................: {valorMultaAtraso:C2}");
+
+Console.WriteLine();
+
+valorProximaFatura = valorNaoPago + jurosSobreRotativo + valorIOFDiario + valorIOFMensal + valorMultaAtraso;
+Console.WriteLine($"Valor a pagar na próxima fatura...: {valorProximaFatura:C2}");
+
+custoCreditoRotativo = valorProximaFatura - valorNaoPago;
+Console.WriteLine($"Custo do crédito rotativo.........: {custoCreditoRotativo:C2}");
--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # CreditoRotativo
 https://github.com/ermogenes/aulas-programacao-csharp/blob/master/exercises/numeros-operadores.md#exerc%C3%ADcio-creditorotativo
+
+---
+
+## Exercício `CreditoRotativo`
+
+Em uma fatura de cartão de crédito a operadora oferta um crédito rotativo, permitindo que o cliente efetue um pagamento inferior ao valor total, mas cobrando o restante na próxima fatura com juros.
+
+Faça um programa que receba o valor total da fatura, o percentual para o pagamento mínimo e a taxa de juros mensais. Exiba o valor do pagamento mínimo, o valor não pago, os juros sobre o rotativo, o valor a ser pago na próxima fatura e o custo do crédito rotativo (valor excedente em relação ao pagamento na fatura corrente). Considere os custos de IOF (Imposto sobre Operações Financeiras) de 0,38% ao mês e 0,0082% ao dia (use 30 dias).
+
+Exemplo:
+
+```
+--- Crédito Rotativo de Cartão de Crédito ---
+
+Valor total da fatura (R$)...: 1000
+Pagamento mínimo (%).........: 15
+Taxa de juros mensais (%)....: 9
+
+Pagamento mínimo..................: R$150,00
+
+Caso seja pago o valor mínimo:
+
+Valor não pago....................: R$850,00
+Juros.............................: R$76,50
+IOF mensal........................: R$3,23
+IOF diário........................: R$2,09
+
+Valor a pagar na próxima fatura...: R$931,82
+Custo do crédito rotativo.........: R$81,82
+```
+
+---
